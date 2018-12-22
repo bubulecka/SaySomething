10e47bbc1bf7e258608c2378f97cf77c1ed99083
bubulecka <bubulecka@users.noreply.github.com>
2018-12-22 04:12:24 +0100

    Added basic pages. Added Not Found page. Added elementary routing. Added jquery.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import 'jquery';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    NotFoundPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
